a59e125f2a5f3666dd392a0c0ee397fa51dd964f
sadik <po480373@gmai.com>
2022-05-10 21:15:18 +0600

    banner and info card added in haomepage

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Banner from "./Banner";
+import Info from "./Info";
 
 const Home = () => {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <>
+    <Banner/>
+    <Info />
+    </>
   );
 };
 
