ed1fabbfb65b894f6a7ab326e315c322f3ba8a4e
sadik <po480373@gmai.com>
2022-05-10 12:24:01 +0600

    text change

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-accent ml-3">Hello</button>
-      <button className="btn btn-accent ml-3">Hello</button>
-      <button className="btn btn-accent ml-3">Hello</button>
+      <button className="btn btn-accent ml-3"> hello from here</button>
+      <button className="btn btn-accent ml-3"> hello from here</button>
+      <button className="btn btn-accent ml-3"> hello from here</button>
     </div>
   );
 }
