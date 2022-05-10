7912fa48ce3fed00f7b00ac6e1db71dcd6c427c5
sadik <po480373@gmai.com>
2022-05-10 12:12:48 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-secondary ml-3">Hello</button>
+      <button className="btn btn-secondary ml-3">Hello</button>
+      <button className="btn btn-secondary ml-3">Hello</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#a991f7",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
